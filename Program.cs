6b4c332aeee1de5cab881ff59878e43794a0aa9c
Mujgan Cihan <mujgancihan88@gmail.com>
2021-03-16 10:24:05 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeriousProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
